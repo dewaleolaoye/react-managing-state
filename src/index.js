695cb407cb97fe82f4cc02f64639a8f7b542ea73
dewaleolaoye <adewaleolaoye21@gmail.com>
2020-01-10 15:55:12 +0100

    connected to redux

--- src/index.js
+++ src/index.js
@@ -1,18 +1,38 @@
 import React, { Component } from 'react';
+import './styles.scss';
 import { render } from 'react-dom';
 
 import { createStore } from 'redux';
 import { connect, Provider } from 'react-redux';
 
-import './styles.scss';
+const initialState = {
+  count: 0,
+};
 
+const INCREMENT = 'INCREMENT';
+
+const incrementValue = () => ({
+  type: INCREMENT,
+});
+
+const reducer = (state = initialState, action) => {
+  if (action.type === INCREMENT) {
+    return { count: state.count + 1 };
+  }
+  return state;
+};
+
+const store = createStore(reducer);
 class Counter extends Component {
   render() {
+    const { count, increment } = this.props;
+    // console.log({ count, increment });
     return (
       <main className="Counter">
-        <p className="count">0</p>
+        <p className="count">{count}</p>
+        {console.log(count)}
         <section className="controls">
-          <button>Increment</button>
+          <button onClick={increment}>Increment</button>
           <button>Decrement</button>
           <button>Reset</button>
         </section>
@@ -21,4 +41,23 @@ class Counter extends Component {
   }
 }
 
-render(<Counter />, document.getElementById('root'));
+const mapStateToProps = state => {
+  return state;
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    increment() {
+      dispatch(incrementValue());
+    },
+  };
+};
+
+const CounterContainer = connect(mapStateToProps, mapDispatchToProps)(Counter);
+
+render(
+  <Provider store={store}>
+    <CounterContainer />
+  </Provider>,
+  document.getElementById('root'),
+);
